498b4b610092476a0ca96f202f5a702a853a60c3
AlexChirkov <tektonikboy98@gmail.com>
2021-09-23 17:40:09 +0300

    Fixed permissions

--- apps/posts/models.py
+++ apps/posts/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth import get_user_model
 
@@ -8,6 +9,11 @@ class Tag(models.Model):
     name = models.CharField(
         max_length=255,
         db_index=True,
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True, default=None
     )
 
     def __str__(self):
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -67,6 +67,22 @@ class PostCreateFormView(generic.FormView):
         return super(PostCreateFormView, self).form_invalid(form)
 
 
+class PostUpdateView(generic.UpdateView):
+    model = Post
+    fields = ('title', 'description', 'tags', )
+    template_name = 'post_update.html'
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('posts:post_list')
+
+    def form_valid(self, form):
+        user = self.request.user
+        if user and self.get_object().user == user:
+            form.save(commit=True)
+        else:
+            return HttpResponse('You have no permissions.')
+        return super(PostUpdateView, self).form_valid(form)
+
+
 class PostDeleteView(generic.DeleteView):
     model = Post
     success_url = reverse_lazy('posts:post_list')
@@ -97,6 +113,22 @@ class CommentDeleteView(generic.DeleteView):
             return HttpResponse('You have no permissions')
 
 
+class CommentUpdateView(generic.UpdateView):
+    model = Comment
+    fields = ('text', )
+    template_name = 'comment_update.html'
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('posts:post_list')
+
+    def form_valid(self, form):
+        user = self.request.user
+        if user and self.get_object().user == user:
+            form.save(commit=True)
+        else:
+            return HttpResponse('You have no permissions.')
+        return super(CommentUpdateView, self).form_valid(form)
+
+
 class TagListView(generic.ListView):
     template_name = 'tags_list.html'
     model = Tag
@@ -113,11 +145,33 @@ class TagCreateFormView(generic.FormView):
     success_url = reverse_lazy('posts:tag_list')
 
     def form_valid(self, form):
-        form.save(commit=True)
+        user = self.request.user
+        if user:
+            tag = form.save(commit=True)
+            tag.user = user
+            tag.save()
+        else:
+            return HttpResponse('You have no permissions.')
         return super(TagCreateFormView, self).form_valid(form)
 
     def form_invalid(self, form):
         return super(TagCreateFormView, self).form_invalid(form)
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ('name', )
+    template_name = 'tag_update.html'
+    template_name_suffix = '_update_form'
+    success_url = reverse_lazy('posts:tag_list')
+
+    def form_valid(self, form):
+        user = self.request.user
+        if user and self.get_object().user == user:
+            form.save(commit=True)
+        else:
+            return HttpResponse('You have no permissions.')
+        return super(TagUpdateView, self).form_valid(form)
 
 
 class TagDeleteView(generic.DeleteView):
@@ -126,7 +180,7 @@ class TagDeleteView(generic.DeleteView):
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_superuser:
+        if self.get_object().user == self.request.user:
             return super(TagDeleteView, self).dispatch(
                 request, *args, **kwargs
             )
